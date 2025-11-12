6fe75de4ab39d37b91a3ce43d62a6924ff9c6c1d
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-11-13 00:59:00 +0530

    my first mobile application

---
+++ mobile-app-1/app/docs.tsx
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const docs = () => {
+  return (
+   <View style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    }}>
+      <Text>Hello this is docs screen</Text>
+    </View>
+  )
+}
+export default docs
+
